ad65ce0a1758e19ce6f51ec9ca8ad368dbf746f9
Duy Anh <timdeveloper97@gmail.com>
2023-05-15 15:42:49 +0700

    add default configuration

---
+++ Data/Configurations/HotelConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelAPI.Data.Configurations
+{
+    public class HotelConfiguration : IEntityTypeConfiguration<Hotel>
+    {
+        public void Configure(EntityTypeBuilder<Hotel> builder)
+        {
+            builder.HasData(new Hotel
+            {
+                Id = 1,
+                Name = "Sapa",
+                Address = "Sapa",
+                CountryId = 1,
+                Rating = 4.5
+            },
+            new Hotel
+            {
+                Id = 2,
+                Name = "Ha Noi",
+                Address = "Ha Noi",
+                CountryId = 2,
+                Rating = 1.5
+            },
+            new Hotel
+            {
+                Id = 3,
+                Name = "Lao Cai",
+                Address = "Lao Cai",
+                CountryId = 3,
+                Rating = 3.5
+            });
+        }
+    }
+}
--- Data/HotelDbContext.cs
+++ Data/HotelDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HotelAPI.Data.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelAPI.Data
@@ -16,49 +17,9 @@ namespace HotelAPI.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Country>().HasData(new Country
-            {
-                Id = 1,
-                Name = "Jamaica",
-                ShortName = "JM",
-            },
-            new Country
-            {
-                Id = 2,
-                Name = "Bahamas",
-                ShortName = "BS",
-            },
-            new Country
-            {
-                Id = 3,
-                Name = "Jenifer",
-                ShortName = "JF",
-            });
-
-            modelBuilder.Entity<Hotel>().HasData(new Hotel
-            {
-                Id = 1,
-                Name = "Sapa",
-                Address = "Sapa",
-                CountryId = 1,
-                Rating = 4.5
-            },
-            new Hotel
-            {
-                Id = 2,
-                Name = "Ha Noi",
-                Address = "Ha Noi",
-                CountryId = 2,
-                Rating = 1.5
-            },
-            new Hotel
-            {
-                Id = 3,
-                Name = "Lao Cai",
-                Address = "Lao Cai",
-                CountryId = 3,
-                Rating = 3.5
-            });
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new CountryConfiguration());
+            modelBuilder.ApplyConfiguration(new HotelConfiguration());
         }
     }
 }
